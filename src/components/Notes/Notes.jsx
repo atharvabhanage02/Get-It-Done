67235dd051fbbc4c66da0534203141d62bb95539
atharvabhanage02 <atharvabhanage2002@gmail.com>
2022-05-31 20:26:48 +0530

    added Pin-Note Feature

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -2,16 +2,20 @@ import "./notes.css";
 import { NotesCard } from "../Notes-Card/NotesCard";
 import { NotesDisplay } from "../NotesTemplate/NotesDisplay";
 import { useNotes } from "../../Context/NotesContext/NotesContext";
-
+import { PinnedNotes } from "../PinnedNotes/PinnedNotes";
 export const Notes = () => {
   const { notesList } = useNotes();
+  const unPinnedNotes = notesList.filter(
+    (item) => !item.isPin && !item.inTrash
+  );
   return (
     <div className="main-content">
       <NotesCard />
+      <PinnedNotes />
       <div className="all-notes-display">
         <h1>All Notes</h1>
         <div className="unpinned-notes">
-          {notesList.map((singleNote) => {
+          {unPinnedNotes.map((singleNote) => {
             return <NotesDisplay key={singleNote._id} notesData={singleNote} />;
           })}
         </div>

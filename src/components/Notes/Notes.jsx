ee75ac3c56e052c51644a0957c692b888f0be309
Atharva Bhanage <atharvabhanage2002@gmail.com>
2022-08-08 20:12:20 +0530

    chore : basic fixes

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -3,18 +3,20 @@ import { NotesCard } from "../Notes-Card/NotesCard";
 import { NotesDisplay } from "../NotesTemplate/NotesDisplay";
 import { PinnedNotes } from "../PinnedNotes/PinnedNotes";
 import { useFilters } from "../../Context/FilterContext/FilterContext";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 export const Notes = () => {
   const { finalFilteredNotes } = useFilters();
   const unPinnedNotes = finalFilteredNotes.filter(
     (item) => !item.isPin && !item.inTrash
   );
+  const [parent] = useAutoAnimate();
   return (
     <div className="main-content">
       <NotesCard />
       <PinnedNotes />
       <div className="all-notes-display">
         <h1>All Notes</h1>
-        <div className="unpinned-notes">
+        <div className="unpinned-notes" ref={parent}>
           {unPinnedNotes.map((singleNote) => {
             return <NotesDisplay key={singleNote._id} notesData={singleNote} />;
           })}
